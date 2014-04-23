ccc332f162d037152c4d3a4dc7e4795b1260005e
UB Space Bulls <sbrover@SBRover.(none)>
2014-04-23 16:01:44 -0400

    asd

--- Camera Switch test/pubcon.py
+++ Camera Switch test/pubcon.py
@@ -8,32 +8,25 @@ def talker():
     rospy.init_node('pubcon', anonymous=True)
     #a=str(0)
     #r = rospy.Rate(10) # 10hz
-    cam0=str(0)
-    cam1=str(0)
-    cam2=str(0)
-    cam3=str(0)
-    cam4=str(0)
+    cam=str(0)
     while not rospy.is_shutdown():
-        cam0=0
-        cam1=0
-        cam2=0
-        cam3=0
-        cam4=0
         CameraNo=input("Enter the camera number:")
         height= input("Enter the image height:")
         width=input("Enter the image width:")
         fps=input("Enter the frame rate:")
         if CameraNo==0:
-            cam0=1
+            cam=0
         if CameraNo==1:
-            cam1=1
+            cam=1
         if CameraNo==2:
-            cam2=1
+            cam=2
         if CameraNo==3:
-            cam3=1
+            cam=3
         if CameraNo==4:
-            cam4=1
-        string =str(cam0)+","+str(cam1)+","+str(cam2)+","+str(cam3)+","+str(cam4)+","+str(fps)+","+str(width)+","+str(height)
+            cam=4
+        if CameraNo==5:
+            cam=5
+        string =str(cam)+","+str(fps)+","+str(width)+","+str(height)
         print string
         #rospy.loginfo(str)
         pub.publish(string)
